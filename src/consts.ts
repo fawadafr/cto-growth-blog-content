ba9a62123cb41c8f233c6abca3f95c880bd7892b
fawadafr <fawadafr@gmail.com>
2025-08-12 12:24:10 -0700

    test: final staging deployment test

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Fawad Rashidi';
-export const SITE_DESCRIPTION = 'Growth Through Transformation [STAGING-GROWTH]';
+export const SITE_DESCRIPTION = 'Growth Through Transformation [STAGING READY]';
