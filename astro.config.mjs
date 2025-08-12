9a4516a4daca3d7aa2c0abf23bd4d34b27930043
fawadafr <fawadafr@gmail.com>
2025-08-12 13:04:40 -0700

    feat: add Astro Vercel adapter for optimized deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,11 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://growth.fawad.ai',
-	integrations: [mdx(), sitemap()],
+  site: 'https://growth.fawad.ai',
+  integrations: [mdx(), sitemap()],
+  adapter: vercel(),
 });
